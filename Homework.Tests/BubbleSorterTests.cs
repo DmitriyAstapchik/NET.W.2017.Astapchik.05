8cb7d5fc7da56dc371dbb7a0e56e4c9371c968a4
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-10-29 14:05:42 +0300

    Add files via upload

---
+++ Homework.Tests/BubbleSorterTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using static Homework.BubbleSorter;
+
+namespace Homework.Tests
+{
+    [TestFixture]
+    public class BubbleSorterTests
+    {
+        int[][] testArray =
+                {
+                  new int[] { 1, 2, 0, 10, 22, -92 },
+                  new int[] { 22, 999, 10, -2, -92, 0, 0, 29 },
+                  new int[] { 19, 04, -2, 021, -12 },
+                  new int[] { 1, 50, 41 },
+                  new int[] { -11, -22, 10, 200, 2 },
+                  new int[] { 123, 123, 490, -24, -29, 0, 12 },
+                  new int[] { 0, 0, 25, 10 }
+                };
+
+        [Test]
+        public void SortBySumAscendingTest()
+        {
+            Sort(testArray, SortBy.Sum, SortOrder.Ascending);
+            Assert.True(testArray.OrderBy(row => row.Sum()).SequenceEqual(testArray));
+        }
+
+        [Test]
+        public void SortByMaxAscendingTest()
+        {
+            Sort(testArray, SortBy.Max, SortOrder.Ascending);
+            Assert.True(testArray.OrderBy(row => row.Max()).SequenceEqual(testArray));
+        }
+
+        [Test]
+        public void SortByMinAscendingTest()
+        {
+            Sort(testArray, SortBy.Min, SortOrder.Ascending);
+            Assert.True(testArray.OrderBy(row => row.Min()).SequenceEqual(testArray));
+        }
+
+        [Test]
+        public void SortBySumDescendingTest()
+        {
+            Sort(testArray, SortBy.Sum, SortOrder.Descending);
+            Assert.True(testArray.OrderByDescending(row => row.Sum()).SequenceEqual(testArray));
+        }
+
+        [Test]
+        public void SortByMaxDescendingTest()
+        {
+            Sort(testArray, SortBy.Max, SortOrder.Descending);
+            Assert.True(testArray.OrderByDescending(row => row.Max()).SequenceEqual(testArray));
+        }
+
+        [Test]
+        public void SortByMinDescendingTest()
+        {
+            Sort(testArray, SortBy.Min, SortOrder.Descending);
+            Assert.True(testArray.OrderByDescending(row => row.Min()).SequenceEqual(testArray));
+        }
+    }
+}
---
+++ Homework/BubbleSorter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework
+{
+    /// <summary>
+    /// The class allows to sort jagged array of integers in several possible ways using bubble sort
+    /// </summary>
+    public static class BubbleSorter
+    {
+        /// <summary>
+        /// Possible kinds of sort
+        /// </summary>
+        public enum SortBy { Sum, Max, Min };
+
+        /// <summary>
+        /// Ascending or descending sort order
+        /// </summary>
+        public enum SortOrder { Ascending, Descending };
+
+        /// <summary>
+        /// Sorts jagged array by ascending/descending its nested arrays elements sum, max value or minvalue
+        /// </summary>
+        /// <param name="array">array to sort</param>
+        /// <param name="by">kind of sort</param>
+        /// <param name="order">order of sort</param>
+        public static void Sort(int[][] array, SortBy by, SortOrder order)
+        {
+            #region compare func initialization
+            Func<int[], int[], bool> compare;
+            if (order == SortOrder.Ascending)
+            {
+                if (by == SortBy.Sum)
+                {
+                    compare = (arr1, arr2) => arr1.Sum() > arr2.Sum();
+                }
+                else if (by == SortBy.Max)
+                {
+                    compare = (arr1, arr2) => arr1.Max() > arr2.Max();
+                }
+                else
+                {
+                    compare = (arr1, arr2) => arr1.Min() > arr2.Min();
+                }
+            }
+            else
+            {
+                if (by == SortBy.Sum)
+                {
+                    compare = (arr1, arr2) => arr1.Sum() < arr2.Sum();
+                }
+                else if (by == SortBy.Max)
+                {
+                    compare = (arr1, arr2) => arr1.Max() < arr2.Max();
+                }
+                else
+                {
+                    compare = (arr1, arr2) => arr1.Min() < arr2.Min();
+                }
+            }
+            #endregion
+
+            bool swapped = true;
+            for (int i = 0; i < array.Length - 1 && swapped == true; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (compare(array[j], array[j + 1]))
+                    {
+                        SwapArrays(ref array[j], ref array[j + 1]);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Swaps two arrays by ref
+        /// </summary>
+        /// <param name="array1">first array</param>
+        /// <param name="array2">second array</param>
+        private static void SwapArrays(ref int[] array1, ref int[] array2)
+        {
+            var temp = array1;
+            array1 = array2;
+            array2 = temp;
+        }
+    }
+}
